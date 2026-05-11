8012c95e4902fac7f64a9372f0d7e0f62c4dd105
Uday Limbadiya <Udaylimbadiya58@gmail.com>
2026-05-11 16:07:23 +0530

    Fix Vercel backend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,24 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const bookingRoutes =
+require("./routes/bookingRoutes");
+
+const authRoutes =
+require("./routes/authRoutes");
+
+const serviceRoutes =
+require("./routes/serviceRoutes");
+
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(
+process.env.MONGO_URI
+)
 .then(() => {
   console.log("MongoDB Connected");
 })
@@ -18,26 +30,30 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.get("/", (req, res) => {
 
-  res.json({
+  res.status(200).json({
+
     success: true,
-    message: "Backend Running Successfully",
+
+    message:
+      "QuickBook Backend Running",
+
   });
 
 });
 
-app.get("/api/bookings", async (req, res) => {
+app.use(
+"/api/bookings",
+bookingRoutes,
+);
 
-  res.json({
-    success: true,
-    message: "Bookings API Working",
-  });
+app.use(
+"/api/auth",
+authRoutes,
+);
 
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+app.use(
+"/api/services",
+serviceRoutes,
+);
 
 module.exports = app;
